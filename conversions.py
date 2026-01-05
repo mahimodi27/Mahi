09d39d6f9b3af70e241184040cfc408fc82f8034
Mahi Modi <modimahi27@gmail.com>
2026-01-05 17:44:18 +0530

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,18 @@
+dollar= int(input("enter dollar"))
+rup= dollar*48
+print(rup)
+
+rup= int(input("enter rup"))
+dollar= rup/48
+print(dollar)
+
+grams= int(input("enter grams"))
+kg=grams/1000
+print(kg)
+
+
+kg= int(input("enter kg"))
+grams=kg*1000                      
+print(grams)
+
+
---
+++ py 1.py
@@ -0,0 +1,34 @@
+x= int(input("enter x"))
+y= int(input("enter y"))
+z=x+y
+print(z)
+
+x= int(input("enter x"))
+y= int(input("enter y"))
+z=x-y
+print(z)
+
+
+x= int(input("enter x"))
+y= int(input("enter y"))
+z=x*y
+print(z)
+
+x= int(input("enter x"))
+y= int(input("enter y"))
+z=x/y
+print(z)
+
+hour =int(input("enter the number of hours"))
+min= hour*60
+print(min)
+
+
+min =int(input("enter the number of mins"))
+hour= min/60
+print(hour)        
+
+
+
+
+
